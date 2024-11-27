fcdcca1bf15b9afd572e31b63eb6f55a4a4cd65e
TexerKanay <aizenznal@gmail.com>
2024-11-27 13:13:32 +0600

    Initial commit

---
+++ src/kg/my/game/template/Magic.java
@@ -0,0 +1,11 @@
+package kg.my.game.template;
+public class Magic extends Hero {
+    public Magic(String name, int health, int damage) {
+        super(name, health, damage, SuperAbility.BOOST);
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        // TODO Here will be implementation of BOOSTING
+    }
+}
---
+++ src/kg/my/game/template/SuperAbility.java
@@ -0,0 +1,8 @@
+package kg.my.game.template;
+
+public enum SuperAbility {
+    CRITICAL_DAMAGE,
+    BOOST,
+    HEAL,
+    BLOCK_DAMAGE_AND_REVERT
+}
